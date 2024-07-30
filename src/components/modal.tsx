ee0490eee210309baa23e4cd58836485442cc80a
ishwor <prataya2@gmail.com>
2024-07-30 17:38:11 +0545

    design update

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -35,7 +35,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-[999]">
       <div
         ref={modalRef}
-        className="bg-white rounded-lg shadow-lg p-12 w-11/12 lg:min-w-md relative"
+        className="bg-white rounded-lg shadow-lg p-12 w-11/12 lg:min-w-md lg:w-auto relative"
       >
         <button
           className="absolute top-2 right-2 text-gray-500 hover:text-primary"
